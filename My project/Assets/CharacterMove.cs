dc2e6a8d55e43467b71817c37b07823a2767325d
QuinnB11 <butcher.github@gmail.cm>
2021-12-05 15:39:42 -0500

    Making some slight error fixes

--- My project/Assets/CharacterMove.cs
+++ My project/Assets/CharacterMove.cs
@@ -4,11 +4,11 @@ using System.Collections;
 public class CharacterMove : MonoBehaviour
 {
     public CharacterController controller;
-    public float speed = 20.0f;
+    public float speed = 15.0f;
     public float gravity = -9.81F;
     Vector3 velocity;
     public Transform groundCheck;
-    public float groundDistance = 0.4f;
+    public float groundDistance = 0.8f;
     public float deathDistance = 0.2f;
     public LayerMask groundMask;
     bool isGrounded;
@@ -39,7 +39,7 @@ public class CharacterMove : MonoBehaviour
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            velocity.y = Mathf.Sqrt(1.5f * -2f * gravity);
+            velocity.y = Mathf.Sqrt(1.5f * -2f * gravity); 
 
         }
 
