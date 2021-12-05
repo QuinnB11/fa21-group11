8876ca58c98c11934eb04dd592c553b61d8c9563
QuinnB11 <butcher.github@gmail.cm>
2021-12-05 13:35:28 -0500

    Character Movement and Camera placement. Test Level Creation

---
+++ My project/Assets/CharacterMove.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bf532befb7752b1cb1da1017da302199725acd1b4ca005816e44fc3db93bc0fe
+size 1159
---
+++ My project/Assets/MouseLook.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a566ca39f84b18673f140470cc71dd89718d827f5d55ab7f07ee866d9685cc4d
+size 1083
