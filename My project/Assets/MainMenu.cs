8accb1c6193c7cf85fff05cb8a855426b76ac954
dwlee0802 <dwlee0802@gmail.com>
2022-01-26 23:41:57 -0500

    Added script for main menu.

---
+++ My project/Assets/MainMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayNewGame()
+    {
+        // SceneManager.LoadScene("");
+        Debug.Log("Start new game");
+    }
+
+    public void PlaySavedGame()
+    {
+        //SceneManager.LoadScene("");
+        Debug.Log("Load savegame");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit game");
+    }
+
+    public void PlayTestLevel()
+    {
+        SceneManager.LoadScene("SampleScene");
+        Debug.Log("Load test level");
+    }
+}
