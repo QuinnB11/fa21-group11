37a9b7959d5e7e000d7c9c93a260f4b36188d780
QuinnB11 <butcher.github@gmail.cm>
2022-04-19 17:22:05 -0400

    AI scene

---
+++ My project/Assets/Script/Controllers/EnemyController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a98ec1cdbd092fdbc3328d1f05a6689cf9be1bbacd8a770415c35847acdf6843
+size 1291
---
+++ My project/Assets/Script/PlayerManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a0e2a752a6816654446c9b3a6ba275d478274a02354c618218ed142f1552d587
+size 290
